c8ea6dccf3b3a57d28a29b37f0754adc7d37fc43
woseibonsu <40476387+woseibonsu@users.noreply.github.com>
2018-10-05 03:04:57 -0400

    Finished (6 Warnings)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        bankAccount myAccount = new bankAccount(0.0, "Winston Osei-Bonsu");
+        myAccount.deposit(505.22);
+        System.out.println(myAccount.balance);
+        myAccount.withdraw(100.0);
+        System.out.println("The " + myAccount.name + " account balance is " + myAccount.balance);
     }
 }
