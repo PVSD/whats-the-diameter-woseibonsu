6d0d8aba758f3d0ccc7730372761eadce1ba522f
woseibonsu <40476387+woseibonsu@users.noreply.github.com>
2018-10-07 14:16:44 -0400

    I somehow managed to push the wrong thing lastime???

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        bankAccount myAccount = new bankAccount(0.0, "Winston Osei-Bonsu");
-        myAccount.deposit(505.22);
-        System.out.println(myAccount.balance);
-        myAccount.withdraw(100.0);
-        System.out.println("The " + myAccount.name + " account balance is " + myAccount.balance);
+        Circle cir1 = new Circle(35.5);
+        System.out.println(cir1.diameter());
     }
+
 }
